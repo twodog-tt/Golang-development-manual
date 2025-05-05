efacecf1006093a02bc3ae158559373852098684
twodogtt <twodogtt01@gmail.com>
2025-05-05 16:55:28 +0800

    task1

---
+++ task_1/lc_430/main.go
@@ -0,0 +1,18 @@
+package main
+
+// https://leetcode.cn/problems/flatten-a-multilevel-doubly-linked-list/
+func main() {
+
+}
+
+type Node struct {
+	Val   int
+	Prev  *Node
+	Next  *Node
+	Child *Node
+}
+
+func flatten(root *Node) *Node {
+
+	return nil
+}
